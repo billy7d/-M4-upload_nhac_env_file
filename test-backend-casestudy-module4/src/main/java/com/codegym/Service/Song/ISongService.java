7ecb8ccada463625514684db8ce1a571bde5c845
billy7d <67430052+billy7d@users.noreply.github.com>
2020-10-28 22:42:14 +0700

    Add files via upload

---
+++ test-backend-casestudy-module4/src/main/java/com/codegym/Service/Song/ISongService.java
@@ -0,0 +1,9 @@
+package com.codegym.Service.Song;
+
+import com.codegym.Model.Song;
+import com.codegym.Service.GeneralService;
+import org.springframework.stereotype.Service;
+
+@Service
+public interface ISongService extends GeneralService<Song> {
+}
